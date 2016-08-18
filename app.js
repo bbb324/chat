5f73013142881e6001b8eeede7832a9d35db72a3
John.xie <John.xie@makeblock.com>
2016-08-18 16:41:10 +0800

    页面2

--- app.js
+++ app.js
@@ -4,8 +4,23 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '111111',
+  database: 'test',
+  port: 3306
+})
+conn.connect();
+conn.query('select * from eat', function(err, rows, fields){
+  if(err) throw err;
+  console.log(rows);
+})
+
 app.set('view engine', 'jade');  //因为 node_modules 中 express 是 view engin 而不是 views
 app.set('views', __dirname + '/views');
+
 
 app.use(express.static('public'));
 
