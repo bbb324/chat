50837e45b39e4b0a6c08d5f0171da1582a31df0f
bbb324 <xiejun325@gmail.com>
2016-10-20 17:57:40 +0800

    上传 vue 的学习过程

--- public/js/main4vue.js
+++ public/js/main4vue.js
@@ -4,16 +4,24 @@
 var vm = new Vue({
   el: '#vue-app',
   data: {
-    todos: [{id: 1, title: 'go shoopint'}, {id: 2, title: 'no shopping'}],
-    newTodo: {id: null, title: ''}
+    todos: [{id: 1, title: 'go shoopint', completed: false}, {id: 2, title: 'no shopping', completed: true}],
+    newTodo: {id: null, title: '', completed: false}
   },
   methods: {
     addNewTodo(newTodo) {
       this.todos.push(newTodo);
-      this.newTodo = {id: null, title: ''}
+      this.newTodo = {id: null, title: '', completed: false}
     },
     delTodo(id) {
       this.todos.$remove(id);
+    },
+    todoCompleted(todo){
+      todo.completed = !todo.completed
+    }
+  },
+  computed: {
+    todoCount(){
+      return this.todos.length;
     }
   }
 });
