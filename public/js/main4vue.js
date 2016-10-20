b13852953f3037d885b65206f1852dc9d111a9ea
bbb324 <xiejun325@gmail.com>
2016-10-20 18:34:02 +0800

    vue 第五课，使用 template 来传递data

--- public/js/main4vue.js
+++ public/js/main4vue.js
@@ -1,27 +1,21 @@
 /**
  * Created by junxie on 2016/10/20.
  */
-var vm = new Vue({
+var TodoItems = Vue.extend({
+  template: '#todo-items-template',
+  props: ['todos'],
+  methods: {
+    todoCompleted(todo){
+      todo.completed = !todo.completed;
+    }
+  }
+});
+Vue.component('todo-item', TodoItems);
+
+new Vue({
   el: '#vue-app',
   data: {
     todos: [{id: 1, title: 'go shoopint', completed: false}, {id: 2, title: 'no shopping', completed: true}],
     newTodo: {id: null, title: '', completed: false}
-  },
-  methods: {
-    addNewTodo(newTodo) {
-      this.todos.push(newTodo);
-      this.newTodo = {id: null, title: '', completed: false}
-    },
-    delTodo(id) {
-      this.todos.$remove(id);
-    },
-    todoCompleted(todo){
-      todo.completed = !todo.completed
     }
-  },
-  computed: {
-    todoCount(){
-      return this.todos.length;
-    }
-  }
 });
