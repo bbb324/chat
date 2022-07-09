3e9b5ae579e181ee48ffc64fbce73ef9b9b32838
bbb324 <johnigzie@163.com>
2022-07-09 17:44:58 +0800

    remove useless

---
+++ webpack.config.basic.js
@@ -0,0 +1,77 @@
+/**
+ * Created by junxie on 18/5/27.
+ */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const entryConfig = require('./entry.json');
+
+
+module.exports = {
+    entry: {
+        'babel-polyfill': 'babel-polyfill',
+        ...entryConfig
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    optimization: {
+        minimizer: [
+            // we specify a custom UglifyJsPlugin here to get source maps in production
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                uglifyOptions: {
+                    compress: false,
+                    ecma: 6,
+                    mangle: true
+                },
+                sourceMap: true
+            })
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: 'index.html'
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"production"'
+        }),
+    ],
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(less|css)$/,
+                use: ['style-loader', 'css-loader', 'less-loader'],
+            },
+            {
+                test: /\.js$/,
+                loader: 'eslint-loader',
+                enforce: 'pre',
+                include: [path.resolve(__dirname, 'src')],
+                options: {
+                    formatter: require('eslint-friendly-formatter')
+                }
+            }
+        ]
+    },
+    resolve: {
+        // you can now require('file') instead of require('file.coffee')
+        extensions: ['.js', '.json', '.jsx']
+    }
+};
